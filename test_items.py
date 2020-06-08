67c0bfd6e5fea4cc0d564662e7c2d6698e53feb3
BogomolovaAS <zf2070406@yandex.ru>
2020-06-08 13:58:18 +0700

    Выполнение задания шага 3.6

---
+++ test_items.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+import time
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_find_button_add_to_basket(browser):
+    link =  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+
+    time.sleep(30)
+    button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "btn-add-to-basket")))
+    type_button = button.get_attribute("type")
+    assert type_button == "submit", "Тип элемента отличный от submit"
+
+
+
